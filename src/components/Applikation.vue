4dd8900b46fefd38d9a955335f1285789b57369a
Casper <csl@netcompany.com>
2021-01-07 13:35:07 +0100

    Updated Applikation.vue with more examples

--- src/components/Applikation.vue
+++ src/components/Applikation.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="applikation-container">
-    <h1>ERST applikation</h1>
+    <h1>Jeg er en h1</h1>
+    <h2>Jeg er en h2</h2>
+    <h3>Jeg er en h3</h3>
+    <h4>Jeg er en h4</h4>
+    <h5>Jeg er en h5</h5>
     {{currentTime}}
     <hr>
     Dynamisk komponent:
@@ -10,28 +14,48 @@
     <div class="spinner" v-if="loadingResponse" aria-label="Henter indhold" />
     {{response}}
     <hr>
-    <button class="button button-primary">Jeg er en knap</button>
+    <button class="button">Jeg er en knap</button>
     <hr>
-    <div class="accordion-wrapper">
-      <ul class="accordion">
-        <li>
-          <button class="accordion-button" aria-expanded="false"
-                  aria-controls="a1">
-            Lorem ipsum dolor sit amet
-            <span class="accordion-icon">
-                  <span class="icon_text">Information</span>
-                  <svg class="icon-svg" focusable="false" aria-hidden="true"><use xlink:href="#info"></use></svg>
-              </span>
-          </button>
-          <div id="a1" aria-hidden="true" class="accordion-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-        </li>
-      </ul>
+    <div class="row">
+      <div class="col-md-6 col-xs-12 align-self-baseline">
+        <h1>Applikationer</h1>
+        <p>
+          Med applikationer forstås den software, der reelt driver jeres virksomhed. Derfor er applikationerne ofte dér, hvor hackere søger hen, når
+          de søger efter informationer eller skal finde en indgang til at sprede ondsindede aktiviteter.
+        </p>
+        <p>
+          Nogle hackere deler også websider, dokumentfiler og andet indhold fra egne hjemmesider eller andre troværdige kilder. Og når umistænksomme
+          ofre besøger indholdet med en ikke-sikret browser eller andet software, lukker de dermed hackeren ind på deres enhed og i sidste ende ind
+          i virksomheden.
+        </p>
+        <p>I det følgende er der to generelle spørgsmål til applikationer, og herefter risikovurderer du jeres individuelle systemer.</p>
+      </div>
+      <div class="col-md-6 col-xs-12 align-self-baseline">
+        <h2>Sådan gør du</h2>
+        <p>Du skal vurdere sandsynlighed og konsekvens på to forskellige skalaer, som hver har fem svarmuligheder. Skalaerne ser ud som følger.</p>
+        <div class="accordion-wrapper">
+          <ul class="accordion accordion-multiselectable">
+            <li>
+              <button class="accordion-button" aria-expanded="false" aria-controls="a1">Skala for sansynlighed</button>
+              <div id="a1" aria-hidden="true" class="accordion-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </li>
+            <li>
+              <button class="accordion-button" aria-expanded="false" aria-controls="a2">Skala for konsekvens</button>
+              <div id="a2" aria-hidden="true" class="accordion-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
     <hr>
     <div class="card">
@@ -94,7 +118,5 @@ export default class Applikation extends Vue {
 }
 </script>
 <style lang="scss" scoped>
-  h1 {
-    color: $color-link;
-  }
+
 </style>
